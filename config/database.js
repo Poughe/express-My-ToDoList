8b347334037aa64f1a011cee327982682e82c457
Ernest Pough <poughe@gmail.com>
2021-10-25 10:50:01 -0400

    enabled database

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url': "mongodb+srv://PoughDev:VzSAUxn8Eoxcqhk4@cluster0.f4shf.mongodb.net/toDoList?retryWrites=true&w=majority",
+    'dbName': 'todo'
+};
+
